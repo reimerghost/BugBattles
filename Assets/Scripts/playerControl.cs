5e87e997305206205b9fa5be73b27bfc39e83982
ReimerGhost <rchamale10@gmail.com>
2015-03-15 23:27:28 -0600

    Multitouch listo

--- Assets/Scripts/playerControl.cs
+++ Assets/Scripts/playerControl.cs
@@ -4,11 +4,39 @@ using System.Collections;
 public class playerControl : MonoBehaviour
 {
     public float velocidad;
+    public float grados;
+
+
+    public Texture btnArriba, btnIzquierda, btnDerecha, btnDispara;
+
+    private Rect rArriba = new Rect(200, 200, 75, 75);
+    private Rect rDispara = new Rect(740, 400, 75, 75);
+    private Rect rIzquierda = new Rect(100, 100, 75, 75);
+    private Rect rDerecha = new Rect(120, 400, 75, 75);
+
+    void OnGUI()
+    {
+        if (Input.touchCount > 0)
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                GUI.Label(new Rect(0, (i*10), 100, 50), Input.GetTouch(i).position.ToString());
+
+                if (rIzquierda.Contains(Input.GetTouch(i).position))
+                {
+                    GUI.Label(new Rect(100, (i * 10), 100, 50), "IZQUIERDA");
+                    girarBicho(1f);
+                }
+                if(rArriba.Contains(Input.GetTouch(i).position)){
+                    GUI.Label(new Rect(100, (i*10), 100, 50), "ARRIBA");
+                    moverDelante();
+                }                
+            }
+    }
 
 	// Use this for initialization
 	void Start () {
 	
-	}
+	}       
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,33 +45,41 @@ public class playerControl : MonoBehaviour
 
     void FixedUpdate()
     {
+
+        
+
         float rotar = Input.GetAxis("Horizontal");
         float mover = Input.GetAxis("Vertical");
-        //Vertical
-        if (mover > 0)
-        {
-            moverDelante(velocidad);
-        }
-        //Horizontal
-        if (rotar > 0)
-        {
-            girarBicho(2, -1);
-        }
-        if (rotar < 0)
-        {
-            girarBicho(2,1);
-        }
+            //Vertical
+            if (mover > 0)
+            {
+                moverDelante();
+            }
+            //Horizontal
+            if (rotar > 0)
+            {
+                girarBicho(-1f);
+            }
+            if (rotar < 0)
+            {
+                girarBicho(1f);
+            }
     }
 
-    void moverDelante(float v)
+    public void moverDelante()
     {
-        GetComponent<Rigidbody2D>().velocity = (Vector2)transform.TransformDirection(Vector3.up) * v;
+
+        GetComponent<Rigidbody2D>().velocity = (Vector2)transform.TransformDirection(Vector3.up) * 0.3f * velocidad;
     }
-    void girarBicho(float grados, int dir)
+
+    public void girarBicho(float dir)
     {
-        grados = grados * dir;
-        transform.Rotate(0.0f, 0.0f, grados);
+        float g = grados*dir;
+        transform.Rotate(0.0f, 0.0f, g);
     }
+
+
+
     void disparar()
     {
     }
