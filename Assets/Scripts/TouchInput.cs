64855f8e970d4e92d879627c27b5d03642089d35
ReimerGhost <rchamale10@gmail.com>
2015-03-16 19:07:34 -0600

    multiTouch

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TouchInput : MonoBehaviour {
+
+    Image image;
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();	
+	}
+
+    public void Red()
+    {
+        image.color = Color.red;
+    }
+
+    public void Blue()
+    {
+        image.color = Color.blue;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/escalarGUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class escalarGUI
+{
+
+    public static int optimalHeight = 854;
+    public static int optimalWidth = 480;
+
+    //Metodo para escalar la pantalla a diferentes tipos de pantallas.
+    public static Rect ResizeGUI(Rect _rect)
+    {
+
+        //Nuevo Ancho
+        float FilScreenWidth = _rect.width / optimalHeight;
+        float rectWidth = FilScreenWidth * Screen.width;
+
+        //Nuevo Alto
+        float FilScreenHeight = _rect.height / optimalWidth;
+        float rectHeight = FilScreenHeight * Screen.height;
+
+        //Nueva posición
+        float rectX = getPosX(_rect.x);
+        float rectY = getPosY(_rect.y);
+
+        return new Rect(rectX, rectY, rectWidth, rectHeight);
+    }
+
+    public static float getPosX(float x)
+    {
+        return (x / optimalWidth) * Screen.width;
+    }
+
+    public static float getPosY(float y)
+    {
+        return ((y / optimalHeight) * Screen.height);
+    }
+}
